de8ee87eff0fe9d62d4efc356d53577bcbec4b21
Emmanuel Kusi <emmanuelkusi345@gmail.com>
2023-05-18 17:36:20 +0000

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,12 @@ const Hero = ({location , temperature , sunrise ,  currentDay , country , mode})
   return (
     <div>
       
-      <div className={ !mode ? "container w-[90%] m-auto bg-hero bg-cover bg-center rounded-md p-5 mt-10" : "container w-[90%] m-auto bg-darkMode text-[#fefe] bg-cover bg-center rounded-md p-5 mt-10"}>
+      <div className={ !mode ? "container max-w-[90%] m-auto bg-hero bg-cover bg-center rounded-md p-5 mt-10" : "container w-[90%] m-auto bg-darkMode text-[#fefe] bg-cover bg-center rounded-md p-5 mt-10"}>
        
-       <div className="content flex items-baseline p-5 pt-10 justify-between backdrop-blur-l rounded-sm bg-white/8">
+       <div className="content flex items-baseline p-5 pt-10 justify-between px-5 backdrop-blur-l rounded-sm bg-white/8">
            <div className="temp">
             { !mode ? <BsCloudDrizzle size={60} color="#02041990"/> : <BsCloudDrizzle size={60} color="#D9D9D9"/> }  
-              <h1 className='text-5xl py-2'>{temperature}°C</h1>
+              <h1 className='text-3xl py-2 md:text-5xl'>{temperature}°C</h1>
               <h2 className='text-[20px]'>{location} , {country}</h2>
            </div>
            <div className="t-d text-right  ">
